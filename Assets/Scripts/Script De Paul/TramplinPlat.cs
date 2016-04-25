04852f98e8743cdbbcd4159d6358aabddc225ed7
luka18 <lucas.mgu@gmail.com>
2016-04-25 15:33:22 +0200

    tramplinplatpushage

---
+++ Assets/Scripts/Script De Paul/TramplinPlat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TramplinPlat : MonoBehaviour
+{
+
+    public int force;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            col.rigidbody.velocity = new Vector3(col.rigidbody.velocity.x, 0, col.rigidbody.velocity.z);
+            col.rigidbody.AddForce((transform.up) * force, ForceMode.VelocityChange);
+
+        }
+    }
+}
