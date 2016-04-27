67d78efd8c272b1afd2d6250e3fb2e48676a3e5d
luka18 <lucas.mgu@gmail.com>
2016-04-27 23:17:11 +0200

    push

---
+++ Assets/Scripts/AddForceTop.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceTop : MonoBehaviour {
+    [SerializeField]
+    int force;
+    [SerializeField]
+    bool sens;
+    Vector3 tosend;
+    void Start()
+    {
+        if (sens)
+        {
+            tosend = new Vector3(0, 0.5f, 1);
+        }
+        else
+            tosend = new Vector3(0, 0.5f, -1);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        
+        if(col.transform.tag =="Player")
+        {
+            Rigidbody lol= col.transform.GetComponent<Rigidbody>();
+            lol.AddForce(tosend * force, ForceMode.VelocityChange);
+        }
+    }
+}
